3ea9f1c9cd7a4ecae62d8eb929e575d59fe43f67
Orlando <orlando.espinosa@smartmatic.com>
2018-03-13 16:40:55 -0500

    se modifico el main y ahora solo se imprime hello world

--- Ejemplo/main.cpp
+++ Ejemplo/main.cpp
@@ -2,25 +2,9 @@
 
 using namespace std;
 
-// Base class
-
-
-// Derived class
-class Rectangle: public Shape {
-public:
-    int getArea() {
-        return (width * height);
-    }
-};
-
 int main(void) {
-    Rectangle Rect;
 
-    Rect.setWidth(5);
-    Rect.setHeight(7);
-
-    // Print the area of the object.
-    cout << "Total area: " << Rect.getArea() << endl;
+    cout << "hello world inicial" << endl;
 
     return 0;
 }
